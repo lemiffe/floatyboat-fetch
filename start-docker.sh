fd970c7d82111f9b2d401381b8d950bdefe9706b
Michael Warner <michael.warner@showpad.com>
2017-01-07 13:29:21 +0100

    Fixed start-docker to proxy port 1337 to 5000 for local development

--- start-docker.sh
+++ start-docker.sh
@@ -3,4 +3,4 @@ docker stop floatyboat
 docker rm floatyboat
 docker rmi floatyboat
 docker build -t floatyboat .
-docker run -d -p 1337:1337 --restart always --name floatyboat floatyboat
+docker run -d -p 1337:5000 --restart always --name floatyboat floatyboat
